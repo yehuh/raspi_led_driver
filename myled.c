a861021e7fec9f4c8eb640a3c192a2b64e16ed04
yehuh <yeh_uh@yahoo.com.tw>
2022-02-06 09:54:24 +0000

    gpio driver is done

--- myled.c
+++ myled.c
@@ -1,6 +1,10 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <asm/uaccess.h>
+#include <linux/uaccess.h>
+#include <linux/io.h>
 
 MODULE_AUTHOR("yehuh");
 MODULE_DESCRIPTION("led driver");
@@ -9,9 +13,20 @@ MODULE_VERSION("0.1");
 
 static dev_t dev;
 static struct cdev cdv;
+static struct class *cls = NULL;
+static volatile u32* gpio_base = NULL;
 
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){
-	printk(KERN_INFO "led _write() is called.\n");
+	char c_buff;
+	if(copy_from_user(&c_buff, buf, sizeof(char))){
+		return -EFAULT;
+	}
+	if(c_buff == '0'){
+		gpio_base[10] = 1 << 25;//set 25 th bit in 0x28 (clear output)
+	}else if(c_buff == '1'){
+		gpio_base[7] = 1 << 25; //set 25 th bit in 0x1C (set out put)
+	}
+
 	return 1;
 }
 
@@ -22,6 +37,17 @@ static struct file_operations led_fops = {
 
 static int __init init_mod(void){
 	int retval;
+	u32 gpio_base_addr = 0x3F200000;
+	u8 region_of_map = 0xA0;
+	gpio_base = ioremap_nocache(gpio_base_addr, region_of_map);
+
+	const u32 led = 25;		//gpio 25
+	const u32 index = led/10;	//10 gpio for 4 bytes
+	const u32 shift = (led % 10)*3;	//3 bits for 1 gpio
+	const u32 mask = ~(0x07 << shift);
+	gpio_base[index] = ((gpio_base[index] & mask) | /*clear gpio 25 related bits*/
+		(0x01 << shift)); //set gpio 25 as output
+
 	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
 	if(retval < 0){
 		printk(KERN_ERR "alloc_chrdev fail!!.\n");
@@ -36,11 +62,20 @@ static int __init init_mod(void){
 		return retval;
 	}
 	printk(KERN_INFO "%s is loaded. major:%d.\n",__FILE__,MAJOR(dev));
+
+	cls = class_create(THIS_MODULE,"myled");
+	if(IS_ERR(cls)){
+		printk(KERN_ERR "class_create() fail.");
+		return PTR_ERR(cls);
+	}
+	device_create(cls, NULL, dev, NULL, "myled%d", MINOR(dev));
 	return 0;
 }
 
 static void __exit cleanup_mod(void){
 	cdev_del(&cdv);
+	device_destroy(cls, dev);
+	class_destroy(cls);
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. major:%d.\n",__FILE__,MAJOR(dev));
 }
