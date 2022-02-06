9129627747b840b35aa522ffe302ca32223a5d32
yehuh <yeh_uh@yahoo.com.tw>
2022-02-06 07:18:28 +0000

    driver description, get device

--- myled.c
+++ myled.c
@@ -1,12 +1,29 @@
 #include <linux/module.h>
+#include <linux/fs.h>
+
+MODULE_AUTHOR("yehuh");
+MODULE_DESCRIPTION("led driver");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1");
+
+static dev_t dev;
 
 static int __init init_mod(void)
 {
+	int retval;
+	retval = alloc_chrdev_region(&dev, 0, 1, "myled");
+	if(retval < 0){
+		printk(KERN_ERR "alloc_chrdev fail!!.\n");
+		return retval;
+	}
+	printk(KERN_INFO "%s is loaded. major:%d.\n",__FILE__,MAJOR(dev));
 	return 0;
 }
 
 static void __exit cleanup_mod(void)
 {
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "%s is unloaded. major:%d.\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod);
